28cbbe914d375ca42a521eca1f0f047552333940
QARosa <rosalijustino@hotmail.com>
2024-05-26 14:18:37 -0300

    II- Validações

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,6 +5,7 @@ Cypress.Commands.add('create', (
     cy.visit('/')
     cy.get('#email').type(Cypress.env('USER_EMAIL'))
     cy.get('#password').type(Cypress.env('USER_PASSWORD'), { log: false })//log como false nçao permite visializar a senha
+    cy.get('button[type="submit"]').click()
     cy.get('h1', {timeout: 10000})
     .should('contain', 'Your Notes')
     cy.contains('Create a new note').click()
